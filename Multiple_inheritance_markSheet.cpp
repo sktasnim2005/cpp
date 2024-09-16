01ffeb8bb2cf56d739efa5cf43dafd9e9bc8776d
Sk Tasnim Ur Rahman <162748135+sktasnim2005@users.noreply.github.com>
2024-09-16 17:05:51 +0600

    Add files via upload

--- Multiple_inheritance_markSheet.cpp
+++ Multiple_inheritance_markSheet.cpp
@@ -38,8 +38,12 @@ public:
 };
 
 int main() {
-    Marksheet ob;
-    ob.student("John ",21);
-    ob.marks(80,85,90);
+    Marksheet ob,ob2;
+    cout<<"Student 1 : "<<endl;
+    ob.student("Tasnim ",1460);
+    ob.marks(100,85,90);
+    cout<<"Student 2 : "<<endl;
+    ob2.student("Alex ",1234);
+    ob2.marks(90,81,96);
     return 0;
 }
