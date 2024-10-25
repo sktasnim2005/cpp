cb41f2c3df999fc476388100726d7307bc767766
Sk Tasnim Ur Rahman <162748135+sktasnim2005@users.noreply.github.com>
2024-10-26 03:13:09 +0600

    Update Delete a data from an Array.cc

--- Delete a data from an Array.cc
+++ Delete a data from an Array.cc
@@ -2,20 +2,17 @@
 using namespace std;
 int main()
 {
+        cout<<"Select size of Array : ";
         int n;
         cin>>n;
         int a[n];
+        cout<<"Enter value of Array : ";
         for(int i=0;i<n;i++){
           cin>>a[i];
         }
         int k;
         cout<<"Value of k : ";
         cin>>k;
-        /*
-        for(int i=k;i<n-1;i++){
-            a[i]=a[i+1];
-        }
-        */
 
         for(int i=k;i<n-1;i++){
             a[i]=a[i+1];
@@ -25,6 +22,7 @@ int main()
             a[i+1]=a[i];
         }
         */
+        cout<<"Final Array : ";
         for(int i=0;i<n-1;i++){
           cout<<a[i]<<" ";
         }
